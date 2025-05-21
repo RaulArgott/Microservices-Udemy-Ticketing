50a80fdc91885a13999e17e86f33fe8e322754b7
Raul Argott <raulargott@hotmail.com>
2025-05-20 21:59:40 -0600

    feat: add common package

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,7 @@ import jwt from "jsonwebtoken";
 
 import { Password } from "../services/password";
 import { User } from "../models/users";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@ratickets1/common";
 
 const router = express.Router();
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { validateRequest } from "../middlewares/validate-request";
 import { User } from "../models/users";
-import { BadRequestError } from "../errors/bad-request-error";
+import { BadRequestError, validateRequest } from "@ratickets1/common";
 
 const router = express.Router();
 
