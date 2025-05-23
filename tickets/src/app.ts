9c87a154edd6f5698b9dab3752b41d08ad8b4551
Raul Argott <raulargott@hotmail.com>
2025-05-22 21:18:56 -0600

    feat: add tickets index endpoint

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-
 import { currentUser, errorHandler, NotFoundError } from "@ratickets1/common";
+
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -18,6 +20,8 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async () => { throw new NotFoundError() });
 
