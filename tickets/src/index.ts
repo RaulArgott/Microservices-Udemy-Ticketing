a327871919c02c723e5d1123551778a1fbcd9b51
Raul Argott <raulargott@hotmail.com>
2025-06-02 21:58:38 -0600

    feat: nats added to ticketing

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 /**
  * Start the server by connecting to the MongoDb and
  * listening on port 3000.
@@ -15,8 +16,22 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
   }
+/*   if(!process.env.NATS_CLIENT_ID){
+    throw new Error("NATS_CLIENT_ID must be defined");
+  } */
+/*   if(!process.env.NATS_URL){
+    throw new Error("NATS_URL must be defined");
+  } */
+  // if(!process.env.NATS_CLUSTER_ID){
+  //   throw new Error("NATS_CLUSTER_ID must be defined");
+  // }
 
   try {
+    await natsWrapper.connect(
+      'ticketing',
+      'abc',
+      'http://nats-srv:4222'
+    );
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDb");
   } catch (err) {
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from "@ratickets1/common";
 import { body } from 'express-validator';
+
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
+
 const router = express.Router();
 
 router.post(
@@ -17,6 +21,13 @@ router.post(
         const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
         await ticket.save();
         res.status(201).send(ticket);
+
+        await new TicketCreatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId,
+        });
     }
 );
 
