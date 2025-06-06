ea29d9a96355c1fd7201cfbe523607fd5fc16dc8
Raul Argott <raulargott@hotmail.com>
2025-06-06 17:19:41 -0600

    feat: add publishers to routes order service

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,23 +1,30 @@
 import express, { Request, Response } from 'express';
 import { NotFoundError, requireAuth, NotAuthorizedError } from '@ratickets1/common';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
-    const order = await Order.findById(req.params.orderId);
+    const order = await Order.findById(req.params.orderId).populate('ticket');
 
     if (!order) {
         throw new NotFoundError();
     }
-    if(order.userId !== req.currentUser!.id) {
+    if (order.userId !== req.currentUser!.id) {
         throw new NotAuthorizedError();
     }
 
     order.status = OrderStatus.Cancelled;
     await order.save();
 
-    // TODO: Emit an event to say this order was cancelled
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id
+        }
+    });
 
     res.status(204).send(order);
 });
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,6 +4,8 @@ import { body } from 'express-validator';
 
 import { Ticket } from '../models/ticket';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -47,9 +49,18 @@ router.post('/api/orders',
 
 
         //  Publish an event saying that an order was created
+        new OrderCreatedPublisher(natsWrapper.client).publish({
+            id: order.id,
+            status: order.status,
+            userId: order.userId,
+            expiresAt: order.expiresAt.toISOString(),
+            ticket: {
+                id: ticket.id,
+                price: ticket.price
+            }
+        });
 
         // Send back the status of the order
-
         res.status(201).send(order);
     });
 
