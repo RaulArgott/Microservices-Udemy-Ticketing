9a6439b63324e25a89e67cc73c3da41dd113e105
Raul Argott <raulargott@hotmail.com>
2025-06-16 21:40:13 -0600

    feat: orders service expiration listener

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,28 @@
+import { Listener, ExpirationCompleteEvent, Subjects } from "@ratickets1/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@ratickets1/common";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+        const order = await Order.findById(data.orderId);
+        if (!order) {
+            throw new Error("Order not found");
+        }
+        order.status = OrderStatus.Cancelled;
+        await order.save();
+        new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+        msg.ack();
+    }
+}
